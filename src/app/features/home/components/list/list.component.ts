5412e395449ebeb68902da19e4be5be94d19067a
Cyberkrane <cyberkrane@gmail.com>
2024-11-27 21:29:16 -0300

    feat:  agrega y actualiza la lista

--- src/app/features/home/components/list/list.component.ts
+++ src/app/features/home/components/list/list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IProject } from '../../interfaces/iproject.interface';
 import { ProjectService } from 'src/app/core/global-services/project.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../modal/modal.component';
+
 
 @Component({
   selector: 'projects-list',
@@ -12,7 +15,7 @@ export class ListComponent implements OnInit {
   displayedColumns: string[] = ['name', 'budget', 'priority', 'createdAt', 'actions'];
   dataSource: IProject[] = [];
 
-  constructor(private readonly productService: ProjectService) { }
+  constructor(private readonly productService: ProjectService, private readonly modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.loadAllProjects();
@@ -21,6 +24,16 @@ export class ListComponent implements OnInit {
   loadAllProjects() {
     this.productService.getAllProjects().subscribe(data => {
       this.dataSource = data;
+    });
+  }
+
+  
+  openModal() {
+    const modalRef = this.modalService.open(ModalComponent);
+    modalRef.result.then((result) => {
+      this.loadAllProjects();
+    }).catch((error) => {
+      console.log('Modal cerrado sin enviar datos');
     });
   }
 
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-home',
@@ -11,17 +9,6 @@ export class HomeComponent  {
 
   public toggle: string = 'arrow_forward_ios';
 
-    
-    constructor(private readonly modalService: NgbModal) { }
-
-    openModal() {
-      const modalRef = this.modalService.open(ModalComponent);
-      modalRef.result.then((result) => {
-        console.log('Resultado del formulario:', result);
-      }).catch((error) => {
-        console.log('Modal cerrado sin enviar datos');
-      });
-    }
   toggleMenu(): void {
     this.toggle = this.toggle === 'arrow_back_ios' ? 'arrow_forward_ios' : 'arrow_back_ios';
   }
